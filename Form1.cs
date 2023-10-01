adc1f36e050fab5c6a3f1838c41f2970456f5054
tantowi.setiawan <tantowi.setiawan@binus.ac.id>
2023-10-01 19:09:39 +0700

    Initialized C# Learning Repository

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Basic_Functions
+{
+    public partial class Form1 : Form
+    {
+        public delegate void setForm2(string text);
+        public event setForm2 setform2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textBoxValue = textBox1.Text;
+            setform2?.Invoke(textBoxValue);
+
+
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Basic_Functions
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            Form1 form1 = new Form1();
+
+            form1.setform2 += setLabel;
+        }
+
+        public void setLabel(string text)
+        {
+            Console.WriteLine(text);
+            label.Text = text;
+        }
+    }
+}
